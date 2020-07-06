08a6bd97b1f1069e7760ee6bc300aa6018be9618
Garrett Caudle <gcaudle66@gmail.com>
2020-07-06 06:48:41 -0500

    addsetup and req docs added setup.py and requirements.txt.
    converted to packagaes...dnac_api,api_rw_flask api,ap_rename20

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dino',
+    version='2.0',
+    packages=['api_rw', 'dnac_api', 'ap_rename20', 'test_flask_api1'],
+    url='https://www.github.com/gcaudle66',
+    license='GPL-3.0-or-later',
+    author='Garrett Caudle',
+    author_email='gcaudle66@gmail.com',
+    description='App for Networking with APIs and IOS-XE'
+)
